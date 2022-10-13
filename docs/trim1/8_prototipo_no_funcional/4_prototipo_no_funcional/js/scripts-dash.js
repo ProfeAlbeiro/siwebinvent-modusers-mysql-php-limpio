cfc7aad153ad59bdc533f7ca92baa60d4819389e
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-10-13 12:48:39 -0500

    Construyendo el Prototipo No Funcional

---
+++ docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts-dash.js
@@ -0,0 +1,15 @@
+/* -------------------------------------------------------------------------------- */
+/* DOM: CAPTURADOR DE CLICK EN EL DOM --------------------------------------------- */
+/* -------------------------------------------------------------------------------- */
+hacerClicDash = document.getElementById("contenedor");
+hacerClicDash.addEventListener('click', function (event) {
+    id = event.target.getAttribute("id");    
+    if (id === "btn-menu-lateral") {
+        btnMenuLateral();
+    }
+});
+// Ocultar Panel Lateral
+function btnMenuLateral() {
+    document.getElementById("panel-lateral").classList.toggle('activar-panel');
+    document.getElementById("area_principal").classList.toggle('ampliar-principal');
+}
