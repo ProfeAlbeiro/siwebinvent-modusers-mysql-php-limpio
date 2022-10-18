d1d2f9747fdb4732f67059be927d323d15d5beed
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-10-18 08:12:43 -0500

    Construyendo el Prototipo No Funcional

--- docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts-dash.js
+++ docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts-dash.js
@@ -1,9 +1,22 @@
 /* -------------------------------------------------------------------------------- */
-/* DOM: CAPTURADOR DE CLICK EN EL DOM --------------------------------------------- */
+/* DOM: CAPTURADOR DE CLICK EN EL DOM (Clases) ------------------------------------ */
+/* -------------------------------------------------------------------------------- */
+const navega = document.querySelectorAll(".ocul-navbar");
+const ocultaConClick = function (evento) {    
+    document.getElementById("navbarSupportedContent").classList.toggle('show');
+}
+// navega es un arreglo así que lo recorremos
+navega.forEach(nav => {
+    //Agregar listener
+    nav.addEventListener("click", ocultaConClick);
+});
+
+/* -------------------------------------------------------------------------------- */
+/* DOM: CAPTURADOR DE CLICK EN EL DOM (Identificadores) --------------------------- */
 /* -------------------------------------------------------------------------------- */
 hacerClicDash = document.getElementById("contenedor");
 hacerClicDash.addEventListener('click', function (event) {
-    id = event.target.getAttribute("id");    
+    id = event.target.getAttribute("id");
     if (id === "btn-menu-lateral") {
         btnMenuLateral();
     }
