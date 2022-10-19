023dd0e44c206cef07187ab0d61f4ab006a2ae69
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-10-19 11:41:09 -0500

    Construyendo el Prototipo No Funcional

--- docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts-dash.js
+++ docs/trim1/8_prototipo_no_funcional/4_prototipo_no_funcional/js/scripts-dash.js
@@ -6,10 +6,10 @@ const panel = document.querySelectorAll(".ocul-panel");
 
 // Recorre
 navega.forEach(nav => {    
-    nav.addEventListener("click", ocultaConClick);    
+    nav.addEventListener("click", ocultaNav);    
 });
 panel.forEach(aside => {
-    aside.addEventListener("click", btnMenuLateral);
+    aside.addEventListener("click", ocultaPanel);
 });
 
 /* -------------------------------------------------------------------------------- */
@@ -27,7 +27,7 @@ hacerClicDash.addEventListener('click', function (event) {
 /* FUNCIONES ---------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------------- */
 // Ocultar Barra de Navegación
-function ocultaConClick() {
+function ocultaNav() {
     var item = document.getElementById("panel-lateral");
     var panelActivo = item.classList.contains('activar-panel');    
     document.getElementById("navbarSupportedContent").classList.toggle('show');
@@ -37,11 +37,18 @@ function ocultaConClick() {
 }
 
 // Ocultar Panel Lateral
-function btnMenuLateral() {
+function btnMenuLateral() {    
     document.getElementById("panel-lateral").classList.toggle('activar-panel');
     document.getElementById("config-img").classList.toggle('ocultar-aside');
     document.getElementById("config-text").classList.toggle('ocultar-aside');
     document.getElementById("config").classList.toggle('activar-panel');
     document.getElementById("modulos").classList.toggle('activar-panel');
     document.getElementById("area_principal").classList.toggle('ampliar-principal');
+}
+
+// Ocultar Panel Lateral: Celular
+function ocultaPanel() {
+    if (screen.width < 992) {
+        btnMenuLateral();
+    }
 }
